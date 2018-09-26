9847e4833d7164597138c0a2438df334f00fa891
Anthony Blanton <anthonyrblanton@gmail.com>
2018-09-26 13:23:17 -0400

    added time average and fixed the buffer being overwritten while waiting

--- threadServer.c
+++ threadServer.c
@@ -1,3 +1,13 @@
+/******************************************
+ * Blocking Multithreaded Server
+ * CIS 452-10 Lab 4
+ * 9/27/2018
+ *
+ * Authors:
+ * Ali Scheffler
+ * Anthony Blanton
+ * **************************************/
+
 #include <pthread.h> 
 #include <stdio.h> 
 #include <errno.h> 
@@ -7,41 +17,45 @@
 #include <signal.h>
 
 void* getFile (void* arg); 
-// arguments:    arg is an untyped pointer 
-// returns:         a pointer to whatever was passed in to arg 
-// side effects:  prints a greeting message
 void sigHandlerExit();
 
 #define MAX 64 
 
+//Shared data
 int fileCount = 0;
+int fileReq = 0;
+int simTime = 0;
+
+//Used to lock shared data 
 static pthread_mutex_t lock;
 
 int main() 
 { 
  	pthread_t thread1;  // thread ID holder 
  	int status;         // captures any error code
-	srand(0);
+	srand(0);	    // seed random number generator
+
+	//Install signal handler
 	signal(SIGINT, sigHandlerExit);
 
 	while(1)
 	{
-
-    		printf("Please enter a filename\n");
-    		char buffer[MAX];
+		//Get filename
+    	printf("Please enter a filename\n");
+    	char buffer[MAX];
 		fgets(buffer, MAX, stdin);
 		fflush(stdin);
 
-		void* arg = (void*) buffer;
+		fileReq++;
 
 		// create and start a thread executing the "do_greeting()" function 
-    		if ((status = pthread_create (&thread1, NULL,  getFile, arg) != 0)) { 
-        		fprintf (stderr, "thread create error %d: %s\n", status, strerror(status)); 
-        		exit (1); 
+		// use of strdup from https://stackoverflow.com/q/5687186
+		if ((status = pthread_create (&thread1, NULL,  getFile, (void *)strdup(buffer)) != 0)) { 
+			fprintf (stderr, "thread create error %d: %s\n", status, strerror(status)); 
+			exit (1); 
     	
 		}			
-	
-
+		
 	}
 
 	pthread_mutex_destroy(&lock);
@@ -51,31 +65,37 @@ int main()
 
 void sigHandlerExit()
 {
-	printf("\tTerminating... File count: %d\n", fileCount);
+	printf("\tTerminating...\n");
+	printf("File Requests Received: %d\n", fileReq);
+	printf("File Requests Serviced: %d\n", fileCount);
+	printf("Average lookup time: %f\n", (double) simTime / fileCount);
 	exit(0);
 }
 
 void* getFile (void * arg) 
 {
 
+	//Get random number between 0 and 9
 	int prob = rand() % 10;
+	int sleepTime = 1;
 
 	if(prob < 8)
 	{
-		sleep(1);	
+		sleep(sleepTime);	
 	}
 	else{
-		int sleepTime = rand() % 4;
-		sleep(sleepTime + 7);
+		sleepTime = (rand() % 4) + 7;
+		sleep(sleepTime);
 	}
-	
+
+	//Lock while thread is running critical section	
 	pthread_mutex_lock(&lock);
 	fileCount++;
+	simTime += sleepTime;
 	pthread_mutex_unlock(&lock);
 
-	printf("File found: %s\n", arg);
-
-	
+	printf("File found: %s\n", (char *) arg);
+	free(arg);
 
 	return arg;
 }
